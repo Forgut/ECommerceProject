ed8b272ae15d030f7cdd6831589921e3848f28a4
Stanisław Gilewicz <stanislaw.gilewicz@efitness.com>
2020-05-14 13:53:34 +0200

    Add shoping cart class

---
+++ Ecommerce.Logic/Objects/ShoppingCart.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ecommerce.Logic.Objects
+{
+    public class ShoppingCart
+    {
+        public ShoppingCart(IEnumerable<Item> items)
+        {
+            Items = items;
+        }
+        public IEnumerable<Item> Items { get; set; }
+        public decimal Price { 
+            get
+            {
+                return Items.Sum(x => x.Price);
+            }
+        }
+    }
+}
