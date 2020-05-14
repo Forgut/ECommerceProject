e17f8793f6eed8ae066854294c6d0b4ee54e2e87
Stanisław Gilewicz <stanislaw.gilewicz@efitness.com>
2020-05-14 13:32:31 +0200

    Add items csv, add parser

---
+++ Ecommerce.Logic/DataSource/DataParser.cs
@@ -0,0 +1,68 @@
+﻿using Ecommerce.Logic.Objects;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Ecommerce.Logic.DataSource
+{
+    public class DataParser
+    {
+        private IEnumerable<Item> _items;
+        private Logger _logger = LogManager.GetCurrentClassLogger();
+
+        public IEnumerable<Item> Items
+        {
+            get
+            {
+                if (_items == null)
+                    _items = GetItemsFromCSV();
+                return _items;
+            }
+        }
+
+        private IEnumerable<Item> GetItemsFromCSV()
+        {
+            string textFile = "Items.csv";
+            var items = new List<Item>();
+            if (File.Exists(textFile))
+                foreach (var line in File.ReadAllLines(textFile))
+                {
+                    var item = ParseLine(line);
+                    if (item != null)
+                        items.Add(item);
+                }
+            return items;
+        }
+
+        private Item ParseLine(string line)
+        {
+            try
+            {
+
+                var values = line.Split(';');
+                int.TryParse(values[0], out int id);
+                string name = values[1];
+                string description = values[2];
+                decimal.TryParse(values[3], out decimal price);
+                string photoPath = values[4];
+
+                return new Item()
+                {
+                    ID = id,
+                    Name = name,
+                    Description = description,
+                    PhotoName = photoPath,
+                    Price = price
+                };
+
+            }
+            catch (Exception e)
+            {
+                _logger.Error(e);
+                return null;
+            }
+        }
+    }
+}
--- Ecommerce.Logic/Objects/Item.cs
+++ Ecommerce.Logic/Objects/Item.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Text;
 
-namespace ECommerce.Logic.Models.Home
+namespace Ecommerce.Logic.Objects
 {
-    public class ItemViewModel
+    public class Item
     {
+        public int ID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string PhotoName { get; set; }
         public decimal Price { get; set; }
     }
 }
