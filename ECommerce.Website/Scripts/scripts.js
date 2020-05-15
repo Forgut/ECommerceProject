0920f01e4fcdd7c66de028ef786f24dc5046c357
Stanisław Gilewicz <stanislaw.gilewicz@efitness.com>
2020-05-15 17:51:20 +0200

    Move scripts to another file

---
+++ ECommerce.Website/Scripts/scripts.js
@@ -0,0 +1,21 @@
+﻿
+function AddAndRemoveButtonClicks(cart_amount_id, add_url, remove_url, item_id) {
+    $("#add").click(function () {
+        $.post({
+            url: add_url,
+            data: { id: item_id },
+        });
+        ReloadCartAmonut(cart_amount_id);
+    });
+    $("#remove").click(function () {
+        $.post({
+            url: remove_url,
+            data: { id: item_id },
+        });
+        ReloadCartAmonut(cart_amount_id);
+    });
+}
+
+function ReloadCartAmonut(cart_amount_id) {
+    $(cart_amount_id).load(location.href + " " + cart_amount_id + ">*", "");
+}
