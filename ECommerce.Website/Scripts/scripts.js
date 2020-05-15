7cf173b63449ddfe5a74ad81eceb186fbf17846f
Stanisław Gilewicz <stanislaw.gilewicz@efitness.com>
2020-05-15 23:09:36 +0200

    Fix animated borders

--- ECommerce.Website/Scripts/scripts.js
+++ ECommerce.Website/Scripts/scripts.js
@@ -1,21 +1,31 @@
-﻿
-function AddAndRemoveButtonClicks(add_url, remove_url, item_id) {
+﻿function add_and_remove_button_clicks(add_url, remove_url, item_id) {
     $("#add-"+item_id).click(function () {
         $.post({
             url: add_url,
             data: { id: item_id }
+        }).done(function () {
+            reload_div("#cart-amount-" + item_id)
         });
-        ReloadDiv("#cart-amount-"+item_id);
     });
     $("#remove-"+item_id).click(function () {
         $.post({
             url: remove_url,
             data: { id: item_id }
+        }).done(function () {
+            reload_div("#cart-amount-" + item_id);
         });
-        ReloadDiv("#cart-amount-"+item_id);
     });
 }
 
-function ReloadDiv(div_id) {
+function reload_div(div_id) {
     $(div_id).load(location.href + " " + div_id + ">*", "");
+}
+
+function highlight_div(div_id) {
+    $(div_id).mouseenter(function (event) {
+        this.style.border = 'solid 10px';
+    });
+    $(div_id).mouseleave(function (event) {
+        this.style.border = 'solid 1px';
+    });
 }
